83eb4e5ff4412c9e3aff6a838742f343c121dd94
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2024-06-17 13:41:32 +0800

    upm

--- 制作src分支.bat
+++ 制作src分支.bat
@@ -1,2 +1,36 @@
+@echo off
+set b="version"
+set version ="1"
+REM 获取版本号
+for /f "tokens=1,2* delims=:," %%a in (Assets/IFramework/package.json) do (
+    echo %%a| findstr %b% >nul && (
+       set version=  %%b
+    ) || (
+        @REM echo %%a nnn %b%
+    )
+)
+
+
+set version=%version: =%
+echo on
 git subtree split --prefix=Assets/IFramework --branch src
 git push origin src:src
+git tag %version% src
+git push origin src --tags
+set cur=%~dp0
+set srcPath=%cur%Assets\IFramework
+set dstPath=%cur%IFrameworkForUPM
+rd /s /q %dstPath%\Editor
+rd /s /q %dstPath%\Runtime
+
+echo off
+xcopy /S /Y %srcPath% %dstPath%
+cd IFrameworkForUPM
+echo on
+git add .
+git commit -m %version%
+git push origin main:main
+git tag %version% main
+git push origin main --tags
+
+pause
