068a3ad408ff3773cb11636faab22bf328e0c215
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2024-06-26 10:54:39 +0800

    编辑器工具

--- Assets/Project/Scripts/PanelNames.cs
+++ Assets/Project/Scripts/PanelNames.cs
@@ -1,5 +1,4 @@
 public class PanelNames
 {
-	 public static string Panel01 = "Assets/IFramework/UI/__Example/res/Panel01.prefab";
-	 public static string Panel02 = "Assets/IFramework/UI/__Example/res/Panel02.prefab";
+	 public static string xxx = "Assets/Project/xxx.prefab";
 }
---
+++ Assets/Project/xxxView.cs
@@ -0,0 +1,28 @@
+﻿/*********************************************************************************
+ *Author:         OnClick
+ *Version:        1.0
+ *UnityVersion:   2021.3.33f1c1
+ *Date:           2024-06-26
+*********************************************************************************/
+using static IFramework.UI.UnityEventHelper;
+namespace IFramework
+{
+	public class xxxView : IFramework.UI.UIItemView 
+	{
+//FieldsStart
+
+//FieldsEnd
+		protected override void InitComponents()
+		{
+		//InitComponentsStart
+
+		//InitComponentsEnd
+		}
+		protected override void OnGet()
+		{
+		}
+		public override void OnSet()
+		{
+		}
+	}
+}
