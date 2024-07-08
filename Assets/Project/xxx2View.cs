585289c795678191b66eb0567eac90825027b29e
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2024-07-08 13:06:12 +0800

    代码生成

--- Assets/Project/xxx2View.cs
+++ Assets/Project/xxx2View.cs
@@ -10,13 +10,15 @@ namespace IFramework
 	public class xxx2View : IFramework.UI.MVC.UIView 
 	{
 //FieldsStart
-		private UnityEngine.CanvasRenderer xxx2;
+		private UnityEngine.GameObject heihei_0;
+		private UnityEngine.GameObject heihei_1;
 
 //FieldsEnd
 		protected override void InitComponents()
 		{
 		//InitComponentsStart
-			xxx2 = transform.GetComponent<UnityEngine.CanvasRenderer>();
+			heihei_0 = transform.Find("heihei_0@sm").gameObject;
+			heihei_1 = transform.Find("heihei_1@sm").gameObject;
 
 		//InitComponentsEnd
 		}
