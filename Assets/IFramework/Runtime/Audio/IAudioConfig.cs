c7f2b594a35b01ce3f68084c07c9e5e8376523fe
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2024-08-13 17:42:59 +0800

    声音

---
+++ Assets/IFramework/Runtime/Audio/IAudioConfig.cs
@@ -0,0 +1,22 @@
+﻿/*********************************************************************************
+ *Author:         anonymous
+ *Version:        1.0
+ *UnityVersion:   2021.3.33f1c1
+ *Date:           2024-08-13
+*********************************************************************************/
+using System.Collections.Generic;
+
+namespace IFramework.Audio
+{
+    public interface IAudioConfig
+    {
+        float GetSoundVolume(int id);
+        bool GetSoundLoop(int id);
+        bool GetSoundCover(int id);
+        int GetSoundExistTime(int sound_id);
+        string GetSoundPath(int sound_id);
+        int GetSoundChannel(int sound_id);
+        List<int> GetChannels();
+    }
+
+}
